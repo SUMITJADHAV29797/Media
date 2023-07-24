86ef0467e21cc206f79a51d16eff21cc3050138f
SUMITJADHAV29797 <sumitjadhav29797@gmail.com>
2023-07-24 14:21:45 +0530

    first commit

---
+++ src/store/apis/photosApi.js
@@ -0,0 +1,61 @@
+import { faker } from "@faker-js/faker";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const photosApi = createApi({
+    reducerPath:'photos',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3005'
+    }),
+    endpoints(builder) {
+        return {
+            fetchPhotos: builder.query({
+                providesTags: (result, error, album) => {
+                    const tags = result.map((photo) => {
+                        return { type: 'photo', id: photo.id }
+                    })
+                    tags.push({ type: 'albumPhoto', id: album.id})
+                    return tags;
+                },
+                query: (album) => {
+                    return {
+                        url: '/photos',
+                        method: 'GET',
+                        params: {
+                            albumId: album.id
+                        },
+                    }
+                }
+            }),
+            addPhoto: builder.mutation({
+                invalidatesTags: (result, error, album) => {
+                    return[{ type: 'albumPhoto', id: album.id}]
+                },
+                query: (album) => {
+                    return {
+                        url: '/photos',
+                        method: 'POST',
+                        body: {
+                            albumId: album.id,
+                            url: faker.image.urlLoremFlickr({category:'abstract', width: 150, height: 150}),
+                        },
+                    }
+                }
+            }),
+            removePhoto: builder.mutation({
+                invalidatesTags: (result, error, photo) => {
+                    return[{ type: 'photo', id: photo.id}]
+                },
+                query: (photo) => {
+                    return {
+                        url: `/photos/${photo.id}`,
+                        method: 'DELETE',
+                    }
+                }
+            }), 
+        }
+    }
+})
+
+export const { useFetchPhotosQuery, useAddPhotoMutation, useRemovePhotoMutation } = photosApi;
+
+export { photosApi };
